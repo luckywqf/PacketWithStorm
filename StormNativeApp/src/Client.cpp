bd375956079ac34392d33226b23b133138993d29
luckywqf <luckywqf@sina.com>
2015-01-09 17:08:36 +0800

    modify packet core dump

--- StormNativeApp/src/Client.cpp
+++ StormNativeApp/src/Client.cpp
@@ -89,34 +89,19 @@ int Client::CheckPacketComplete(char *data, int len)
 	}
 
 	struct pcap_pkthdr *header = (struct pcap_pkthdr*)data;
-	assert(header->caplen <= 65535);
 	len -= header->caplen;
 	return len;
 }
 
 RawPacket* Client::NextPacket()
 {
-	static uint64_t receiveLen = 0;
-	static uint64_t dealLen = 0;
 	ssize_t nread;
 	while(true) {
 		int leftLen = CheckPacketComplete(dataBuf_ + dataStart_, currentPos_ - dataStart_);
 		if (leftLen >= 0) {
 			RawPacket *packet = new RawPacket((struct pcap_pkthdr *)(dataBuf_ + dataStart_), (unsigned char*)(dataBuf_ + dataStart_ + sizeof(struct pcap_pkthdr)));
 			int packetLen = sizeof(struct pcap_pkthdr) + packet->pkthdr_.caplen;
-			if (BufferLen - currentPos_ < 8192) { //max pdu length
-				int leftStart = dataStart_ + packetLen;
-				memcpy(dataBuf_, dataBuf_ + leftStart, leftLen);
-				currentPos_ = leftLen; // move to starter
-				dataStart_ = 0;
-			} else {
-				dataStart_ += packetLen;
-			}
-
-//			std::cerr << "leftLen=" << leftLen << ", packetLen=" << packetLen << ", dataStart_=" << dataStart_
-//					<< ", currentPos_=" << currentPos_ << ", receiveLen="<< receiveLen << std::endl;
-			std::cerr << dealLen << " " << packet->pkthdr_.caplen << std::endl;
-			dealLen += packetLen;
+			dataStart_ += packetLen;
 			return packet;
 		} else if (currentPos_ >= BufferLen) {
 			int len = currentPos_ - dataStart_;
@@ -125,13 +110,11 @@ RawPacket* Client::NextPacket()
 			dataStart_ = 0;
 		}
 
-
 		nread = read(fd_, dataBuf_ + currentPos_, BufferLen - currentPos_);
 		if (nread < 0) {
 		   perror("read");
 		   return NULL;
 		}
 		currentPos_ += nread;
-		receiveLen += nread;
 	}
 }
